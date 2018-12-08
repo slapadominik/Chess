194ba1900797f8e7ca3489fe129d2434cb143c28
Dominik Słapa <dominik.slapa@gmail.com>
2018-12-08 17:02:42 +0100

    King movement implemented

--- Chess.API/Chess.Logic/Figures/Bishop.cs
+++ Chess.API/Chess.Logic/Figures/Bishop.cs
@@ -1,31 +1,57 @@
-﻿using Chess.Logic.Interfaces;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chess.Logic.Exceptions;
+using Chess.Logic.Interfaces;
 
 namespace Chess.Logic.Figures
 {
     public class Bishop : Chessman
     {
+        private IEnumerable<int> _validMovesTopLeft;
+        private IEnumerable<int> _validMovesTopRight;
+        private IEnumerable<int> _validMovesDownLeft;
+        private IEnumerable<int> _validMovesDownRight;
+
         public Bishop(Color color) : base(color)
         {
+            _validMovesTopLeft = Enumerable.Range(1, 7).Select(a => a * (-9));
+            _validMovesTopRight = Enumerable.Range(1, 7).Select(y => y * (-7));
+            _validMovesDownLeft = Enumerable.Range(1, 7).Select(z => z * 7);
+            _validMovesDownRight = Enumerable.Range(1, 7).Select(a => a * 9);
         }
 
         public override MoveStatus MakeMove(IBoard board, string @from, string to)
         {
-            throw new System.NotImplementedException();
+            if (board.GetChessman(to) == null)
+            {
+                return MakeNonCaptureMove(board, from, to);
+            }
+
+            return MakeCaptureMove(board, from, to);
+        }
+
+        private MoveStatus MakeNonCaptureMove(IBoard board, string from, string to)
+        {
+            ValidateMove(board, from, to, _validMoves);
+            Move(board, from, to);
+            return MoveStatus.Normal;
+        }
+
+        private MoveStatus MakeCaptureMove(IBoard board, string from, string to)
+        {
+            if (board.GetChessman(to).GetColor() == GetColor())
+            {
+                throw new InvalidMoveException($"Location [{to}] contains friendly chessman!");
+            }
+
+            ValidateMove(board, from, to, _validMoves);
+            Move(board, from, to);
+            return MoveStatus.Capture;
         }
 
         public override bool Equals(object obj)
         {
-            if (Equals(obj))
-            {
-                return true;
-            }
-
-            if (obj is Bishop)
-            {
-                return GetColor() == (obj as Bishop).GetColor();
-            }
-
-            return false;
+            return ReferenceEquals(this, obj);
         }
 
     }
--- Chess.API/Chess.Logic/Figures/King.cs
+++ Chess.API/Chess.Logic/Figures/King.cs
@@ -1,16 +1,49 @@
-﻿using Chess.Logic.Interfaces;
+﻿using Chess.Logic.Exceptions;
+using Chess.Logic.Interfaces;
 
 namespace Chess.Logic.Figures
 {
     public class King : Chessman
     {
+        private int[] _validMoves;
+
         public King(Color color) : base(color)
         {
+            _validMoves = new int[] {-9, -8, -7, -1, 1, 7, 8, 9};
         }
 
         public override MoveStatus MakeMove(IBoard board, string @from, string to)
         {
-            throw new System.NotImplementedException();
+            if (board.GetChessman(to) == null)
+            {
+                return MakeNonCaptureMove(board, from, to);
+            }
+
+            return MakeCaptureMove(board, from, to);
+        }
+
+        private MoveStatus MakeNonCaptureMove(IBoard board, string from, string to)
+        {
+            ValidateMove(board, from, to, _validMoves);
+            Move(board, from, to);
+            return MoveStatus.Normal;
+        }
+
+        private MoveStatus MakeCaptureMove(IBoard board, string from, string to)
+        {
+            if (board.GetChessman(to).GetColor() == GetColor())
+            {
+                throw new InvalidMoveException($"Location [{to}] contains friendly chessman!");
+            }
+
+            ValidateMove(board, from, to, _validMoves);
+            Move(board, from, to);
+            return MoveStatus.Capture;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj);
         }
     }
 }
